6b5d1824bcdf2a918988c9d219b65c641558b5e1
Andoniaina <amrazanadrakoto@gmail.com>
2026-01-04 15:14:47 +0100

    Add library entry point and build config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: "src/index.js",
+      entry: path.resolve(__dirname, "src/index.js"),
       name: "ReactSimpleModal",
-      fileName: (format) => `react-simple-modal.${format}.js`,
+      fileName: "index",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
     },
   },
 });
